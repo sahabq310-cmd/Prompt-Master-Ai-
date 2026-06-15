e4385158adc6ba445f743f38979e46fcff883c5a
sahabq310-cmd <sahabq310@gmail.com>
2026-06-15 14:56:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
 # Model setup
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
